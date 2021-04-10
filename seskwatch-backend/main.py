16139a889c632adcfb9732a65c651b47cc509ddb
Kevin <sumpfmensch@abwesend.de>
2021-04-10 20:18:36 +0200

    add tortoise and move list_sessions and create_session to database

--- seskwatch-backend/main.py
+++ seskwatch-backend/main.py
@@ -2,30 +2,39 @@ from typing import Optional
 from fastapi import FastAPI
 from datetime import datetime
 
-from pydantic import BaseModel
+from tortoise.models import Model
+from tortoise import fields
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
+
 
 app = FastAPI()
 
 db = [] # mock db
 
-class Session(BaseModel):
-    name: str
-    description: str
-    start: datetime
-    duration: int
+class Session(Model):
+    id = fields.UUIDField(pk=True)
+    name = fields.CharField(50)
+    description = fields.CharField(50)
+    start = fields.DatetimeField()
+    duration = fields.IntField(constraints={"minimum": 0})
+
+Session_Pydantic = pydantic_model_creator(Session, name='Session')
+SessionIn_Pydantic = pydantic_model_creator(Session, name='SessionIn', exclude_readonly=True)
+
 
 @app.get("/")
 def index():
     return {''}
 
 @app.get("/sessions")
-def list_sessions():
-    return db
+async def list_sessions():
+    return await Session_Pydantic.from_queryset(Session.all())
 
 @app.post("/sessions")
-def create_session(session: Session):
-    db.append(session.dict())
-    return session.dict()
+async def create_session(session: SessionIn_Pydantic):
+    session_obj = await Session.create(**session.dict()) #exclude_unset=True if optional nullable fields are present
+    return await Session_Pydantic.from_tortoise_orm(session_obj)
 
 @app.delete("/sessions/{session_id}")
 def delete_session(session_id: int):
@@ -34,7 +43,7 @@ def delete_session(session_id: int):
     return res
 
 @app.put("/sessions/{session_id}")
-def edit_session(session_id:int, session: Session):
+def edit_session(session_id:int, session: SessionIn_Pydantic):
     db[session_id] = session
     return  db[session_id].dict()
 
@@ -45,3 +54,12 @@ def register(session_id):
 @app.delete("/sessions/{session_id}/registration")
 def unregister(session_id):
     raise RuntimeError("Not implemented")
+
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
