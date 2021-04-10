6c1366d7dfd7cccb1e976d758a691fdca7d438c0
Kevin <sumpfmensch@abwesend.de>
2021-04-10 18:50:40 +0200

    in memory db - put does not work yet

--- seskwatch-backend/main.py
+++ seskwatch-backend/main.py
@@ -1,31 +1,47 @@
 from typing import Optional
 from fastapi import FastAPI
+from datetime import datetime
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
+db = [] # mock db
+
+class Session(BaseModel):
+    name: str
+    description: str
+    start: datetime
+    duration: int
 
 @app.get("/")
 def index():
     return {''}
 
-
 @app.get("/sessions")
 def list_sessions():
-    return [{"id": '1', "name": "testsession"}]
-
+    return db
 
 @app.post("/sessions")
-def create_session():
-    return [{"id": '1', "name": "testsession"}]
+def create_session(session: Session):
+    db.append(session.dict())
+    return session.dict()
+
+@app.delete("/sessions/{session_id}")
+def delete_session(session_id: int):
+    res = db[session_id-1]
+    db.pop(session_id-1)
+    return res
 
 @app.put("/sessions/{session_id}")
-def edit_session():
-    return [{"id": '1', "name": "testsession"}]
+def edit_session(session_id:int, session: Session):
+    db[session_id-1] = Session
+    return  db[session_id-1]
 
 @app.post("/sessions/{session_id}/registration")
 def register(session_id):
-    return {}
+    raise RuntimeError("Not implemented")
 
 @app.delete("/sessions/{session_id}/registration")
 def unregister(session_id):
-    return {}
+    raise RuntimeError("Not implemented")
